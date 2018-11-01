4726979cb52f5a24a798949701b35d6675eb3060
Siân Culley <s.culley@ucl.ac.uk>
2018-11-01 13:07:13 +0000

    updated nearest neighbours analysis so that execute not empty

--- Core/src/nanoj/core/java/gui/localisation/NearestNeighboursAnalysis_.java
+++ Core/src/nanoj/core/java/gui/localisation/NearestNeighboursAnalysis_.java
@@ -1,10 +1,12 @@
 package nanoj.core.java.gui.localisation;
 
 import ij.ImagePlus;
+import ij.gui.HistogramWindow;
 import ij.gui.NonBlockingGenericDialog;
 import ij.gui.PointRoi;
 import ij.measure.ResultsTable;
 import ij.plugin.filter.MaximumFinder;
+import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import nanoj.core.java.gui._BaseDialog_;
 import nanoj.kernels.Kernel_NearestNeighbour;
@@ -31,6 +33,8 @@ public class NearestNeighboursAnalysis_ extends _BaseDialog_ {
     private Kernel_NearestNeighbour NN = new Kernel_NearestNeighbour();
     private Kernel_VoronoiImage VI = new Kernel_VoronoiImage();
     private double tolerance;
+    private int nBins;
+    private float pixelSize;
 
     @Override
     public boolean beforeSetupDialog(String arg) {
@@ -43,21 +47,73 @@ public class NearestNeighboursAnalysis_ extends _BaseDialog_ {
     public void setupDialog() {
         gd = new NonBlockingGenericDialog("Nearest Neighbour Analysis");
         gd.addNumericField("Tolerance", getPrefs("tolerance", 10), 2);
-        gd.addCheckbox("Show preview", false);
+        gd.addNumericField("Number of bins for histogram", getPrefs("nBins", 100), 0);
+        gd.addNumericField("Pixel size (nm)", getPrefs("pixelSize", 100), 1);
+        gd.addCheckbox("Preview point selection", false);
     }
 
     @Override
     public boolean loadSettings() {
         tolerance = gd.getNextNumber();
+        nBins = (int) gd.getNextNumber();
+        pixelSize = (float) gd.getNextNumber();
         showPreview = gd.getNextBoolean();
 
         setPrefs("tolerance", tolerance);
+        setPrefs("nBins", nBins);
+        setPrefs("pixelSize", pixelSize);
         savePrefs();
         return true;
     }
 
     @Override
     public void execute() throws InterruptedException, IOException {
+
+        imp.deleteRoi();
+
+        ImageProcessor ip = imp.getProcessor();
+
+        Polygon plg = MF.getMaxima(ip, tolerance, true);
+        PointRoi roi = new PointRoi(plg);
+
+        imp.setRoi(roi);
+
+        float[] xps = intToFloat(plg.xpoints);
+        float[] yps = intToFloat(plg.ypoints);
+
+        float[][] nearest = NN.calculate(xps, yps);
+        float[] nearestDistance = nearest[0];
+
+        int nPoints = nearestDistance.length;
+
+        float[] nearestDistanceNm = new float[nPoints];
+        for(int i=0; i<nPoints; i++){
+            nearestDistanceNm[i] = nearestDistance[i]*pixelSize;
+        }
+
+        // Create  table
+        log.status("populating table...");
+        Map<String, double[]> data = new LinkedHashMap<String, double[]>();
+        data.put("x-position", floatToDouble(xps));
+        data.put("y-position", floatToDouble(yps));
+        data.put("closest neighbour distance (pixels)", floatToDouble(nearestDistance));
+        data.put("closest neighbour distance (nm)", floatToDouble(nearestDistanceNm));
+        ResultsTable rt = dataMapToResultsTable(data);
+        rt.show("Nearest-Neighbour Table");
+
+        // Create histogram
+        log.status("creating histogram");
+        FloatProcessor fpHist = new FloatProcessor(nPoints, 1, nearestDistanceNm);
+        ImagePlus impHist = new ImagePlus("List of neighbour distances", fpHist);
+        HistogramWindow histogramWindow = new HistogramWindow("Histogram of nearest-neighbour distances", impHist, nBins);
+
+        // Create Voronoi diagram
+
+        ImageProcessor ipVoronoi = VI.calculateImage(ip.getWidth(), ip.getHeight(), xps, yps, nearestDistanceNm);
+
+        ImagePlus impVoronoi = new ImagePlus("Nearest Neighbours Voronoi (nm)", ipVoronoi);
+
+        impVoronoi.show();
 
     }
 
@@ -69,28 +125,6 @@ public class NearestNeighboursAnalysis_ extends _BaseDialog_ {
             PointRoi roi = new PointRoi(plg);
 
             imp.setRoi(roi);
-
-            float[] xps = intToFloat(plg.xpoints);
-            float[] yps = intToFloat(plg.ypoints);
-
-            float[][] nearest = NN.calculate(xps, yps);
-            float[] nearestDistance = nearest[0];
-
-            // Create  table
-            log.status("populating table...");
-            Map<String, double[]> data = new LinkedHashMap<String, double[]>();
-            data.put("x-position", floatToDouble(xps));
-            data.put("y-position", floatToDouble(yps));
-            data.put("closest neighbour distance", floatToDouble(nearestDistance));
-            ResultsTable rt = dataMapToResultsTable(data);
-            rt.show("Nearest-Neighbour Table");
-
-            ImageProcessor ipVoronoi = VI.calculateImage(ip.getWidth(), ip.getHeight(), xps, yps, nearestDistance);
-
-            if (impPreview != null) impPreview.setProcessor(ipVoronoi);
-            else impPreview = new ImagePlus("Nearest Neighbours Voronoi", ipVoronoi);
-
-            impPreview.show();
         }
     }
 }
